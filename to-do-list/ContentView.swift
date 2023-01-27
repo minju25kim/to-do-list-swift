e8c1db63ff4aae4c95e5476b88126fb1a9b9cd34
Minju25Kim <48757517+minju25kim@users.noreply.github.com>
2023-01-27 11:25:40 +0000

    Initial Commit

---
+++ to-do-list/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  to-do-list
+//
+//  Created by Minju25Kim on 27/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ to-do-list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to-do-list
+//
+//  Created by Minju25Kim on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
